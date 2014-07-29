60cd5322403e01937928b69de194e99dbd320736
Jelizaveta Potapova <jpotapova@Administrators-MacBook-Pro.local>
2014-07-29 06:41:15 +0100

    added another test for sum

--- example1/tests.js
+++ example1/tests.js
@@ -3,5 +3,9 @@ describe("Sum of", function() {
     it("22 and 5 should be 27", function() {
         expect(sum(22, 5)).toBe(27);
     });
-     
+
+    it("14 and 6 should be 20", function() {
+        expect(sum(14, 6)).toBe(20);
+    });
+
 });
